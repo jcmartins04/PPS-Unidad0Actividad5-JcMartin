994813b7cf372bbe5f25d5c6e18e4c354878d6cd
jcmartins04 <jcmartins04@informatica.iesvalledeljerteplasencia.es>
2024-11-12 22:49:24 +0100

    modifiying index.php file because not run properly

--- index.php
+++ index.php
@@ -8,10 +8,10 @@
         foreach( $imgs as $img ) {
             if( $img=="." || $img==".." )
                 continue;
-            if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-3)=="jpeg"){
-                $name = substr($img,0,-4);
-            }else if (substr($img,-4)=="jpeg") {
+            if( substr($img,-3)=="jpg" or substr($img,-3)=="png" or substr($img,-4)=="jpeg"){
                 $name = substr($img,0,-5);
+            }else if (substr($img,-4)=="jpeg") {
+                $name = substr($img,0,-4);
             }
             echo "<a href='profile/$name.html'>";
             echo "<img src='img/$img' width='130'>";
